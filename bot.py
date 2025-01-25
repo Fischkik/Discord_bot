2b23ce67d5f60899299ec24891b1cfcaf2249e71
Fischkik <flashkikblitzblig@gmail.com>
2025-01-25 19:46:13 +0100

    final fixes 1

--- bot.py
+++ bot.py
@@ -1,7 +1,14 @@
 import discord
 from discord.commands import Option
 
+def get_dc_api() -> str:
+    with open("api.txt","r") as api_key:
+        return api_key.readlines()[0]
+        
+
 intents = discord.Intents.default()
+intents.members = True
+
 bot = discord.Bot(
     intents=intents,
     debug_guilds=[1332722155342790759]
@@ -12,10 +19,6 @@ bot = discord.Bot(
 async def on_ready():
     print("bot is working")
 
-bot.run("")
-
-@bot.slash_command(descrption="Moin")
-async def greet(ctx, user: Option(discord.Member, "greetable user")):
-    await ctx.respond(f"Moin {user.mention}")
-    
-bot.run("")
+bot.load_extension("twitch_background")
+bot.load_extension("greet")  
+bot.run(get_dc_api())
--- greet.py
+++ greet.py
@@ -1,7 +1,16 @@
+import discord
 from discord.ext import commands
 from discord.commands import slash_command
 
-class Greet(commands.cog):
+class Greet(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
+    @slash_command()
+    async def greet(self, ctx):
+        await ctx.respond(f"Na du nudel {ctx.author.menton}")
+        
+        
+        
+def setup(bot):
+    bot.add_cog(Greet(bot))
