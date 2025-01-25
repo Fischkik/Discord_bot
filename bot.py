f363a6814b6a1ac1959fb543703d7f3566b0a72c
Fischkik <flashkikblitzblig@gmail.com>
2025-01-25 17:20:31 +0100

    api_key ausgelagert

--- bot.py
+++ bot.py
@@ -1,10 +1,21 @@
 import discord
+from discord.commands import Option
 
-bot = discord.bot(debug_guilds=[1332722155342790759])
+intents = discord.Intents.default()
+bot = discord.Bot(
+    intents=intents,
+    debug_guilds=[1332722155342790759]
+    )
 
 
 @bot.event
 async def on_ready():
     print("bot is working")
 
+bot.run("")
+
+@bot.slash_command(descrption="Moin")
+async def greet(ctx, user: Option(discord.Member, "greetable user")):
+    await ctx.respond(f"Moin {user.mention}")
+    
 bot.run("")
